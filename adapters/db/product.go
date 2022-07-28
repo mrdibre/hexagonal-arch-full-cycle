69b2426a8c9667b3fd64b341a46d8df3d8b00c97
César Felipe <cesar.felp982@gmail.com>
2022-07-28 07:54:20 -0300

    Creating tests for db adapter

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -10,6 +10,10 @@ type ProductDb struct {
 	db *sql.DB
 }
 
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db}
+}
+
 func (db *ProductDb) Get(id string) (application.ProductInterface, error) {
 	var product application.Product
 
@@ -26,4 +30,56 @@ func (db *ProductDb) Get(id string) (application.ProductInterface, error) {
 	}
 
 	return &product, nil
+}
+
+func (db *ProductDb) create(product application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := db.db.Prepare("INSERT INTO products(id, name, price, status) VALUES(?, ?, ?, ?);")
+
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmt.Exec(product.GetID(), product.GetName(), product.GetPrice(), product.GetStatus())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (db *ProductDb) update(product application.ProductInterface) (application.ProductInterface, error) {
+	_, err := db.db.Exec(
+		"UPDATE products SET name = ?, price = ?, status = ? WHERE id = ?",
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+		product.GetID(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (db *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
+	var rows int
+
+	db.db.QueryRow("SELECT id FROM products WHERE id = ?", product.GetID()).Scan(&rows)
+
+	if rows == 0 {
+		_, err := db.create(product)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		_, err := db.update(product)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return product, nil
 }
--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -1,8 +1,12 @@
-package db
+package db_test
 
 import (
 	"database/sql"
+	"github.com/mrdibre/hexagonal-arch-go/adapters/db"
+	"github.com/mrdibre/hexagonal-arch-go/application"
+	"github.com/stretchr/testify/require"
 	"log"
+	"testing"
 )
 
 var Db *sql.DB
@@ -35,4 +39,42 @@ func createProduct(db *sql.DB) {
 	}
 
 	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, application.DISABLED, product.GetStatus())
+}
+
+func TestProductDb_Save(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "Product Test"
+	product.Price = 25
+
+	result, err := productDb.Save(product)
+
+	require.Nil(t, err)
+	require.Equal(t, product.Name, result.GetName())
+	require.Equal(t, product.Price, result.GetPrice())
+	require.Equal(t, product.Status, result.GetStatus())
+
+	product.Enable()
+
+	result, err = productDb.Save(product)
+
+	require.Nil(t, err)
+	require.Equal(t, product.Status, result.GetStatus())
 }
